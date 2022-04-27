abc33e48d8b6abd4a9416af26ef671e0361b50cf
Tej Patel <tejpatel317@gmail.com>
2022-04-27 10:52:52 -0400

    commit tree f48472bf57886de57246046bc1759781bd969272

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+    return arrayOfDrivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+    return arrayOfDrivers.slice(arrayOfDrivers.length-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+    return (fare) => number*fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, firstOrLastTwo) {
+    if (firstOrLastTwo === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    }
+    else if (firstOrLastTwo === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers);
+    }
+}
